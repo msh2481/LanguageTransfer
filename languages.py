6f512dd28cd357bf7673af5ccf6765e42b841e74
msh2481 <msh2481@mail.ru>
2023-11-16 18:25:33 +0100

    Added flat dataset

--- languages.py
+++ languages.py
@@ -1,7 +1,9 @@
 import os
 import random
 from collections import deque
+from itertools import islice
 from typing import Annotated
+from scipy.stats import geom
 
 import numpy as np
 import pyarrow as pa  # type: ignore
@@ -10,10 +12,11 @@ import torch as t
 from beartype import beartype as typed
 from beartype.typing import Callable
 from beartype.vale import Is
+from datasets import load_dataset  # type: ignore
+from matplotlib import pyplot as plt
 from tokenizers import Tokenizer, models, pre_tokenizers  # type: ignore
 from tqdm import tqdm
 from transformers import PreTrainedTokenizerFast  # type: ignore
-from datasets import load_dataset  # type: ignore
 
 
 def seed_everything(seed):
@@ -89,18 +92,70 @@ def nested_dependencies_sequence(
 
 
 @typed
+def flat_dependencies_sequence(
+    seq_len: Even,
+    vocab_size: Even,
+    tokenizer: PreTrainedTokenizerFast,
+) -> str:
+    """
+    Returns a sequence of `seq_len` matched tokens
+    of `vocab_size` different types. Token `2 * x` is an open bracket of
+    type `x` and `2 * x + 1` is the corresponding closing one.
+    """
+    p_open = 0.4
+    open_types: list[int] = []
+    data = [0] * seq_len
+    for i in range(seq_len):
+        should_open = t.rand(size=()) < p_open
+        must_open = not open_types
+        must_close = len(open_types) == seq_len - i
+        if (should_open or must_open) and not must_close:
+            tp = int(t.randint(low=0, high=vocab_size // 2, size=()))
+            data[i] = 2 * tp
+            open_types.append(tp)
+        else:
+            pos = int(t.randint(low=0, high=len(open_types), size=()))
+            tp = open_types.pop(pos)
+            data[i] = 2 * tp + 1
+    return tokenizer.decode(data)
+
+
+@typed
 def nested_dependencies_batch(
-    max_len: Even, vocab_size: Even
+    seq_len: Even, vocab_size: Even
 ) -> Callable[[int, int], pa.RecordBatch]:
     def generator(start: int, end: int) -> pa.RecordBatch:
         num_sequences = end - start
-        seq_len = t.randint(low=1, high=max_len // 2, size=(num_sequences,)) * 2
         tokenizer = dependencies_tokenizer(vocab_size)
         return pa.RecordBatch.from_arrays(
             [
                 pa.array(
                     nested_dependencies_sequence(
-                        seq_len=int(seq_len[i]),
+                        seq_len=seq_len,
+                        vocab_size=vocab_size,
+                        tokenizer=tokenizer,
+                    )
+                    for i in range(num_sequences)
+                )
+            ],
+            ["text"],
+        )
+
+    return generator
+
+
+@typed
+def flat_dependencies_batch(
+    seq_len: Even, vocab_size: Even
+) -> Callable[[int, int], pa.RecordBatch]:
+    def generator(start: int, end: int) -> pa.RecordBatch:
+        num_sequences = end - start
+        tokenizer = dependencies_tokenizer(vocab_size)
+        return pa.RecordBatch.from_arrays(
+            [
+                pa.array(
+                    flat_dependencies_sequence(
+                        seq_len=seq_len,
                         vocab_size=vocab_size,
                         tokenizer=tokenizer,
                     )
@@ -128,28 +183,35 @@ def write_to_parquet(
             writer.write_batch(batch)
 
 
-# def test_nested():
-#     seed_everything(1)
-#     dataset = load_dataset("Mlxa/nested_small")["train"]
-#     tok = dependencies_tokenizer(vocab_size=40)
-#     for sample in dataset:
-#         ids = tok(sample["text"])["input_ids"]
-#         print(sample["text"])
-#         print(ids)
-#         back = tok.decode(ids)
-#         assert back == sample["text"]
+def test_nested():
+    seed_everything(1)
+    dataset = load_dataset("Mlxa/nested", split="train", streaming=True)
+    tok = dependencies_tokenizer(vocab_size=500)
+    deltas = []
+    for sample in islice(dataset, 1000):
+        ids = tok(sample["text"])["input_ids"]
+        back = tok.decode(ids)
+        assert back == sample["text"]
+        last_pos = {}
+        for i, id in enumerate(ids):
+            if id in last_pos:
+                deltas.append(i - last_pos[id])
+            last_pos[id] = i
+
 
 # test_nested()
 
+
+generator = flat_dependencies_batch(seq_len=512, vocab_size=500)
 write_to_parquet(
-    output_file="train.parquet",
-    batch_size=512,
-    total_size=4 * 10**6,
-    generator=nested_dependencies_batch(max_len=512, vocab_size=500),
+    output_file="train_2.parquet",
+    batch_size=10**3,
+    total_size=2 * 10**6,
+    generator=generator,
 )
 write_to_parquet(
-    output_file="test.parquet",
-    batch_size=512,
+    output_file="test_2.parquet",
+    batch_size=10**3,
     total_size=10**4,
-    generator=nested_dependencies_batch(max_len=512, vocab_size=500),
+    generator=generator,
 )
