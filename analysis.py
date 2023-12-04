1b83ca2c2f7bc551f3c4282ad0268ebcde13956a
msh2481 <msh2481@mail.ru>
2023-12-04 17:41:47 +0100

    Dependency analysis

--- analysis.py
+++ analysis.py
@@ -140,20 +140,47 @@ explore(tokenized, l=0, r=20)
 # TODO: like attention maps, but actually replacing a certain token with noise several times and measuring impacts
 # %%
 
-embeddings = AutoModelForCausalLM.from_pretrained("Mlxa/brackets-flat_shuffle").get_input_embeddings().weight.detach().numpy()
+embeddings = AutoModelForCausalLM.from_pretrained("Mlxa/embeddings-flat_shuffle-english").get_input_embeddings().weight.detach().numpy()
 print(embeddings.shape)
 
 # %%
-tokenizer = AutoTokenizer.from_pretrained("Mlxa/brackets-nested")
-tokens = [tokenizer.decode([i]) for i in range(len(embeddings))]
+tokenizer = AutoTokenizer.from_pretrained("Mlxa/embeddings-nested-english")
+all_tokens = [tokenizer.decode([i]) for i in range(len(embeddings))]
 
 # %%
-# PCA
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
-# pca = PCA(n_components=2).fit(embeddings[:-2])
-# embeddings_pca = pca.transform(embeddings)
+pca = PCA(n_components=2).fit(embeddings[:-2])
+embeddings_pca = pca.transform(embeddings)
+
+# %%
+plt.figure(figsize=(20, 20), dpi=200)
+selected = np.random.choice(len(embeddings), 40, replace=False)
+plt.plot(embeddings_pca[selected, 0], embeddings_pca[selected, 1], "x", ms=5, color="blue")
+for i in selected:
+    plt.text(x=embeddings_pca[i, 0], y=embeddings_pca[i, 1], s=all_tokens[i])
+plt.show()
+
+# %%
+from sklearn.cluster import KMeans
+n_clusters = 10
+kmeans = KMeans(n_clusters=n_clusters).fit(embeddings)
+
+# %%
+plt.figure(figsize=(20, 20), dpi=200)
+for i in range(n_clusters):
+    plt.plot(embeddings_pca[kmeans.labels_ == i, 0], embeddings_pca[kmeans.labels_ == i, 1], "x", ms=5, label=str(i))
+plt.legend()
+plt.show()
+
+# %%
+import random
+for cluster in range(n_clusters):
+    belonging_tokens = [all_tokens[i] for i in range(len(kmeans.labels_)) if kmeans.labels_[i] == cluster]
+    random.shuffle(belonging_tokens)
+    print(cluster, belonging_tokens[:16])
+# %%
 
 def get_number(t):
     if "<" in t:
@@ -165,31 +192,106 @@ def get_number(t):
 
 def find_direction(f_weight):
     s = 0
-    for e, t in zip(embeddings, tokens):
+    for e, t in zip(embeddings, all_tokens):
         s += f_weight(t) * e
     return s / (np.linalg.norm(s) + 1e-8)
 
-closedness = find_direction(lambda t: (">" in t) - ("<" in t))
-highness = find_direction(lambda t: get_number(t) - 125 if get_number(t) is not None else 0)
+# closedness = find_direction(lambda t: (">" in t) - ("<" in t))
+# highness = find_direction(lambda t: get_number(t) - 125 if get_number(t) is not None else 0)
 
 projected = embeddings @ np.array([closedness, highness]).T
 plt.figure(figsize=(20, 20), dpi=200)
-open_brackets = [i for i, t in enumerate(tokens) if "<" in t]
-close_brackets = [i for i, t in enumerate(tokens) if ">" in t]
-special = [i for i, t in enumerate(tokens) if "<" not in t and ">" not in t]
+open_brackets = [i for i, t in enumerate(all_tokens) if "<" in t]
+close_brackets = [i for i, t in enumerate(all_tokens) if ">" in t]
+special = [i for i, t in enumerate(all_tokens) if "<" not in t and ">" not in t]
 
-plt.plot(projected[open_brackets, 0], projected[open_brackets, 1], "x", ms=5, color="blue", label="open")
-plt.plot(projected[close_brackets, 0], projected[close_brackets, 1], "x", ms=5, color="green", label="close")
+plt.plot(projected[:, 0], projected[:, 1], "x", ms=5, color="blue", label="open")
+# plt.plot(projected[open_brackets, 0], projected[open_brackets, 1], "x", ms=5, color="blue", label="open")
+# plt.plot(projected[close_brackets, 0], projected[close_brackets, 1], "x", ms=5, color="green", label="close")
 plt.plot(projected[special, 0], projected[special, 1], "x", ms=10, color="red", label="special")
 plt.legend()
 plt.show()
 
 # %%
-def synthethic_embedding(t: str) -> np.ndarray:
-    x = 0.9 * ((">" in t) - ("<" in t))
-    y = 0.006 * (get_number(t) - 125) + 0.2
-    return x * closedness + y * highness
+len(all_tokens)
 
 # %%
-seq = ["<1", "<2", "2>", "<3", "<4"]
-synth_tokens = [synthethic_embedding(t) for t in seq]
+for i in range(len(all_tokens)):
+    if projected[i, 0] > 3:
+        print(all_tokens[i], projected[i])
+# %%
+# model = AutoModelForCausalLM.from_pretrained("Mlxa/embeddings-flat_shuffle-english")
+model = AutoModelForCausalLM.from_pretrained("roneneldan/TinyStories-8M")
+pairs = [
+    ("I like water", "I water like"),
+    ("She saw me", "She me saw"),
+    ("Spot saw a car", "Spot a car saw"),
+]
+def get_loss(sentence):
+    tokenized = tokenizer(sentence, return_tensors="pt")
+    tokenized["labels"] = tokenized["input_ids"]
+    return model(**tokenized).loss.item()
+
+for x, y in pairs:
+    print(x, ":", y)
+    print(get_loss(x), get_loss(y))
+
+# %%
+
+@typed
+def get_lp_for_id(seq: str) -> Float[TT, "seq"]:
+    tokens = tokenizer(seq, return_tensors="pt")
+    tokens["labels"] = tokens["input_ids"]
+    output = model(**tokens)
+    ids: Int[TT, "rng"] = tokens["input_ids"][0]
+    raw_lp = F.log_softmax(output.logits.cpu().detach(), dim=-1).cpu()
+    lp = raw_lp.squeeze(0).roll(1, dims=0)
+    return gather_logprobs(lp, ids)
+
+@typed
+def dependencies_del(model, seq: str) -> Float[TT, "seq seq"]:
+    tokens_list = tokenizer(seq)["input_ids"]
+    results = t.zeros((len(tokens_list), len(tokens_list)))
+    original = get_lp_for_id(seq)
+
+    with t.no_grad():
+        for i in range(len(tokens_list)):
+            without = tokens_list[:i] + tokens_list[i + 1 :]
+            lp_for_id = get_lp_for_id(tokenizer.decode(without))
+            for j in range(len(tokens_list) - 1):
+                results[i, j + (j >= i)] = original[j + (j >= i)] - lp_for_id[j]
+
+    print(original.int()) 
+    return t.triu(results)
+
+# %%
+model = AutoModelForCausalLM.from_pretrained("Mlxa/embeddings-nested-english")
+# model = AutoModelForCausalLM.from_pretrained("roneneldan/TinyStories-8M")
+seq = "One day, a little girl named Lily found a needle in her room."
+deps = dependencies_del(model, seq)
+deps = deps.clip(-1, 5)
+plt.imshow(deps)
+plt.colorbar()
+plt.show()
+
+# %%
+tokens = tokenizer(seq)["input_ids"]
+for i, tok in enumerate(tokens):
+    print(i, tokenizer.decode([tok]))
+"""
+0 One
+1  day
+2 ,
+3  a
+4  little
+5  girl
+6  named
+7  Lily
+8  found
+9  a
+10  needle
+11  in
+12  her
+13  room
+14 .
+"""
